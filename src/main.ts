b138d2e8c35da72f9fce49625ccc9259f4a995fa
Juan Camilo Acevedo <juan.acevedo@finaktiva.com>
2024-03-18 22:06:53 -0500

    feat: addling config initial projec

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  const options = new DocumentBuilder()
+    .setTitle('superFlight API')
+    .setDescription('Scheduled Flight App')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
